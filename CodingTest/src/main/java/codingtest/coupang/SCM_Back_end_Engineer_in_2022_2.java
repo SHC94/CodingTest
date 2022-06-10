6302606f01d35d99313143f7e96e9f853a6aad09
SHC <gudcjf4757@naver.com>
2022-06-10 23:45:45 +0900

    틀린 문제에 대한 원인 파악

--- CodingTest/src/main/java/codingtest/coupang/SCM_Back_end_Engineer_in_2022_2.java
+++ CodingTest/src/main/java/codingtest/coupang/SCM_Back_end_Engineer_in_2022_2.java
@@ -16,6 +16,7 @@ public class SCM_Back_end_Engineer_in_2022_2 {
 	public static long[] solution(long n) {
         long[] answer = new long[2];
         List<Long> list = new ArrayList<Long>();
+        /**에라토스테네스 체로 값을 셋팅*/
         
         for(int i = 1 ; i * i <= n ; i++){
         	
@@ -23,6 +24,7 @@ public class SCM_Back_end_Engineer_in_2022_2 {
         		Long x = (long) i;
         		Long y = (long) (n / i);
         		
+        		/**셋팅된 값으로 소수 판별*/
         		if(x != y && isPrime(x, y)) {
         			list.add(x);
         			list.add(y);
@@ -48,19 +50,19 @@ public class SCM_Back_end_Engineer_in_2022_2 {
 		if(x == 1 || x == 0) return false;
 		if(y == 1 || y == 0) return false;
 		
-		for (int i = 2 ; i <= (int)Math.sqrt(x) ; i++) {
+		for (long i = 2 ; i <= Math.sqrt(x) ; i++) {
             if (x % i == 0) {
                return false;
             }//end if()
         }//end for()
 
-		for (int i = 2 ; i <= (int)Math.sqrt(y) ; i++) {
+		for (long i = 2 ; i <= Math.sqrt(y) ; i++) {
             if (y % i == 0) {
                return false;
             }//end if()
         }//end for()
 		
-         return true;
+        return true;
 	}//end isPrime()
 
 }//end class()
--- CodingTest/src/main/java/codingtest/coupang/SCM_Back_end_Engineer_in_2022_3.java
+++ CodingTest/src/main/java/codingtest/coupang/SCM_Back_end_Engineer_in_2022_3.java
@@ -7,11 +7,11 @@ public class SCM_Back_end_Engineer_in_2022_3 {
 	
 	public static void main(String[] args) {
 		
-		int[][] board		= {	{3, 2, 3, 2},
-								{2, 1, 1, 2},
-								{1, 1, 2, 1},
-								{4, 1, 1, 1}
-							};
+//		int[][] board		= {	{3, 2, 3, 2},
+//								{2, 1, 1, 2},
+//								{1, 1, 2, 1},
+//								{4, 1, 1, 1}
+//							};
 		
 //		int[][] board		= {	{4, 2, 3, 2},
 //								{2, 1, 2, 4},
@@ -19,26 +19,34 @@ public class SCM_Back_end_Engineer_in_2022_3 {
 //								{4, 1, 4, 3}
 //							};
 
+		/**반례*/
+		int[][] board		= {	{1, 1, 1, 1},
+								{1, 1, 1, 1},
+								{1, 1, 1, 1},
+								{1, 1, 1, 1}
+							};
+
+		
+		/**DFS로 풀어볼 것.*/
 		int result 			= solution(board);
 		
 		System.out.println(result);
 	}//end main()
-	//
 	
 	
 	public static int solution(int[][] board) {
         int max = -1;
-        
+         
         for(int a = 0 ; a < board.length ; a++) {
         	for(int b = 0 ; b < board[a].length ; b++) {
         		//좌표 지점마다 반복
-        		int gigun = board[a][b];
+        		int gigun = board[0][0];
         		Queue<Coordinate> queue = new LinkedList<Coordinate>();
-        		queue.add(new Coordinate(a, b, 1));
+        		queue.add(new Coordinate(0, 0, 1));
         		int[][] pattern			= {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         		
         		boolean[][] visited 	= new boolean[board.length][board[0].length];
-                visited[a][b] 			= true;
+                visited[0][0] 			= true;
                 
                 while(!queue.isEmpty()) {
                 	
@@ -47,6 +55,7 @@ public class SCM_Back_end_Engineer_in_2022_3 {
                 	int y = coordinate.y;
                 	int distance = coordinate.distance;
                 	
+                	System.out.println(" ============== ");
                 	for(int q = 0 ; q < pattern.length ; q++) {
                 		int moveX = pattern[q][0] + x;
                 		int moveY = pattern[q][1] + y;
@@ -57,8 +66,9 @@ public class SCM_Back_end_Engineer_in_2022_3 {
                 		
                 		visited[moveX][moveY] = true;
                 		queue.add(new Coordinate(moveX, moveY, distance + 1));
-                		int dis = queue.peek().distance;
+                		int dis = distance + 1;
                 		if(max < dis) max = dis;
+                		System.out.println(moveX + "\t" + moveY + "\t" + dis);
                 	}//end for()
                 }//end while()
                 
