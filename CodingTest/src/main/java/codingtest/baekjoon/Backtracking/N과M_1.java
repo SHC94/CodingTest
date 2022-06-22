770573191c22dbedff9d0ed573ca566accca32d9
SHC94 <gudcjf4757@naver.com>
2022-06-22 16:24:25 +0900

    Backtracking 1. N과M_1
    BruteForce
    1. 덩치
    2. 일곱난쟁이

---
+++ CodingTest/src/main/java/codingtest/baekjoon/Backtracking/N과M_1.java
@@ -0,0 +1,57 @@
+package codingtest.baekjoon.Backtracking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class N과M_1 {
+	
+	private static List<String> result = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+
+		for(int a = 0 ; a < N ; a++) arr[a] = a + 1;
+		//1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+		
+		permutation(arr, 0, arr.length, M);
+		
+		Collections.sort(result);
+		for(String answer : result) {
+			System.out.println(answer);
+		}//end for()
+	}//end main()
+
+	private static void permutation(int[] arr, int depth, int n, int r) {
+		if(depth == r) {
+			print(arr, r);
+			return;
+		}//end if()
+       
+		for(int i = depth ; i < n; i++) {
+			swap(arr, depth, i);
+			permutation(arr, depth + 1, n, r);
+			swap(arr, depth, i);
+		}//end for()
+	}//end permutation()
+
+	private static void print(int[] arr, int r) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < r; i++) {
+            sb.append(arr[i]).append(" ");
+		}//end for()
+         
+		result.add(sb.toString());
+	}//end permuprint()
+
+	private static void swap(int[] arr, int depth, int i) {
+		int temp	= arr[depth];
+        arr[depth]	= arr[i];
+        arr[i]		= temp;
+	}//end swap()
+
+}//end class()
---
+++ CodingTest/src/main/java/codingtest/baekjoon/BruteForce/덩치.java
@@ -0,0 +1,40 @@
+package codingtest.baekjoon.BruteForce;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 덩치 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] arr = new int[N][2];
+		int[] result = new int[N];
+		Arrays.fill(result, 1);
+		
+		for(int a = 0 ; a < N ; a++) {
+			arr[a][0] = sc.nextInt();
+			arr[a][1] = sc.nextInt();
+		}//end for()
+		
+		
+		for(int a = 0 ; a < arr.length - 1; a++) {
+			for(int b = a + 1 ; b < arr.length ; b++) {
+				if((arr[b][0] < arr[a][0]) && (arr[b][1] < arr[a][1])) {
+					result[b]++;
+				} else if((arr[a][0] < arr[b][0]) && (arr[a][1] < arr[b][1])) {
+					result[a]++;
+				}
+			}//end for()
+		}//end for()
+		
+		for(int a = 0 ; a < result.length ; a++) {
+			if(a != result.length - 1) {
+				System.out.print(result[a] + " ");
+			} else {
+				System.out.print(result[a]);
+			}
+		}//end for()
+	}//end main()
+
+}//end class()
