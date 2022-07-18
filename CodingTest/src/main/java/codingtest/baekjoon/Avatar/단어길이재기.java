6f21a16f340bd3265d42ac01c23e24c36aceded1
SHC94 <gudcjf4757@naver.com>
2022-07-18 16:25:22 +0900

    1.단어길이재기 2.단어정렬 3.알파벳개수

---
+++ CodingTest/src/main/java/codingtest/baekjoon/Avatar/단어길이재기.java
@@ -0,0 +1,13 @@
+package codingtest.baekjoon.Avatar;
+
+import java.util.Scanner;
+
+public class 단어길이재기 {
+	
+	public static void main(String[] args) {
+		Scanner sc	= new Scanner(System.in);
+		String S 	= sc.next();
+		System.out.println(S.length());
+	}//end main()
+
+}//end class()
---
+++ CodingTest/src/main/java/codingtest/baekjoon/Avatar/알파벳개수.java
@@ -0,0 +1,37 @@
+package codingtest.baekjoon.Avatar;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class 알파벳개수 {
+	
+	public static void main(String[] args) {
+		Scanner sc 							= new Scanner(System.in);
+		HashMap<String, Integer> resultMap 	= new HashMap<String, Integer>();
+		StringBuilder result 				= new StringBuilder();
+		
+		String S = sc.next();
+		
+		for(int a = 0 ; a < S.length(); a++) {
+			String str = String.valueOf(S.charAt(a));
+			if(resultMap.containsKey(str)) {
+				resultMap.put(str, resultMap.get(str) + 1);
+			} else {
+				resultMap.put(str, 1);
+			}//end if()
+		}//end for()
+
+		for(int a = 97 ; a <= 122 ; a++) {
+			String key = String.valueOf((char) a);
+			if(resultMap.containsKey(key)) {
+				result.append(resultMap.get(key)).append(" ");
+			} else {
+				result.append(0).append(" ");
+			}//end if()
+		}//end for()
+		
+		
+		System.out.println(result.toString());
+	}//end main()
+
+}//end class()
