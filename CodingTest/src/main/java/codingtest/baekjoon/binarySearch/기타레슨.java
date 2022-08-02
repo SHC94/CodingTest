222c5fb999100ac7e469910860b66b85afa239b3
SHC94 <gudcjf4757@naver.com>
2022-08-02 21:49:47 +0900

    1. 기타레슨 2. 암기왕 3. 반도체설계

---
+++ CodingTest/src/main/java/codingtest/baekjoon/binarySearch/기타레슨.java
@@ -0,0 +1,63 @@
+package codingtest.baekjoon.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 기타레슨 {
+	
+	private static int N, M;
+	private static int[] lesson;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] arr 	= br.readLine().split(" ");
+		N 				= Integer.parseInt(arr[0]);
+		M 				= Integer.parseInt(arr[1]);
+		lesson 			= new int[N];
+		int max 		= 0;
+		arr 			= br.readLine().split(" ");
+		
+		for (int i = 0; i < N; i++) {
+			lesson[i] = Integer.parseInt(arr[i]);
+			max = Math.max(max, lesson[i]);
+		}//end for()
+
+		bw.write(search(max, 1000000000) + "\n");
+		bw.flush();
+
+	}//end main()
+
+	public static int search(int left, int right) {
+
+		int pl = left;
+		int pr = right;
+
+		while (pl <= pr) {
+			int pc = (pl + pr) / 2;
+			int cnt = 1;
+			int sum = 0;
+
+			for (int i = 0; i < N; i++) {
+				sum += lesson[i];
+				if (sum > pc) {
+					sum = lesson[i];
+					cnt++;
+				} // end if()
+			} // end for()
+
+			if (cnt > M) {
+				pl = pc + 1;
+			} else {
+				pr = pc - 1;
+			} // end if()
+
+		} // end while()
+
+		return pl;
+	}// end search()
+}//end class()
---
+++ CodingTest/src/main/java/codingtest/baekjoon/binarySearch/암기왕.java
@@ -0,0 +1,45 @@
+package codingtest.baekjoon.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class 암기왕 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw 	= new BufferedWriter(new OutputStreamWriter(System.out));
+		int testCase 		= Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < testCase; i++) {
+			int N 							= Integer.parseInt(br.readLine());
+			StringTokenizer st 				= new StringTokenizer(br.readLine());
+			HashMap<Integer, Boolean> map 	= new HashMap<>();
+			
+			for (int j = 0; j < N; j++) {
+				map.put(Integer.parseInt(st.nextToken()), true);
+			}//end for()
+
+			int M = Integer.parseInt(br.readLine());
+
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				if (map.containsKey(Integer.parseInt(st.nextToken()))) {
+					bw.write("1\n");
+				} else {
+					bw.write("0\n");
+				}//end if()
+			}//end for()
+		}//end for()
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}// end main()
+
+}// end class()
