b13d669ba1aa61d694a55f139acf18375e10281b
SHC94 <gudcjf4757@naver.com>
2022-08-03 16:47:40 +0900

    1. 오르막수 2. 가장긴바이토닉부분수열 3. 가장큰증가부분수열

---
+++ CodingTest/src/main/java/codingtest/baekjoon/DP/가장긴바이토닉부분수열.java
@@ -0,0 +1,50 @@
+package codingtest.baekjoon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 가장긴바이토닉부분수열 {
+
+	private static int[][] dp = new int[30][30];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		int n 				= Integer.parseInt(br.readLine());
+		String[] spl 		= br.readLine().split(" ");
+		int[] list 			= new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			list[i] = Integer.parseInt(spl[i]);
+		}//end for()
+
+		int[] leftlist 	= new int[n];
+		int[] rightlist = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j >= 0; j--) {
+				if (list[i] > list[j]) {
+					leftlist[i] = Math.max(leftlist[i], leftlist[j] + 1);
+				}//end if()
+			}//end for()
+		}//end for()
+		
+		
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j < n; j++) {
+				if (list[i] > list[j]) {
+					rightlist[i] = Math.max(rightlist[i], rightlist[j] + 1);
+				}//end if()
+			}//end for()
+		}//end for()
+
+		int answer = 0;
+		for (int i = 0; i < n; i++) {
+			answer = Math.max(leftlist[i] + rightlist[i], answer);
+		}//end for()
+		
+		System.out.println(answer + 1);
+
+	}// end main()
+
+}// end class()
---
+++ CodingTest/src/main/java/codingtest/baekjoon/DP/가장큰증가부분수열.java
@@ -0,0 +1,41 @@
+package codingtest.baekjoon.DP;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 가장큰증가부분수열 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw 	= new BufferedWriter(new OutputStreamWriter(System.out));
+		int N 				= Integer.parseInt(br.readLine());
+		int dp[]	 		= new int[N + 1];
+		int map[] 			= new int[N + 1];
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+			dp[i] = map[i];
+		}//end for()
+
+		int result = dp[1];
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j < i; j++) {
+				if (map[j] < map[i]) {
+					dp[i] = Math.max(dp[j] + map[i], dp[i]);
+					result = Math.max(result, dp[i]);
+				}//end if()
+			}//end for()
+		}//end for()
+		bw.write(String.valueOf(result));
+		bw.flush();
+		br.close();
+		bw.close();
+	}// end main()
+
+}// end class()
