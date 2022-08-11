d5f0b1265933874cc63ae116606394f789278818
SHC94 <gudcjf4757@naver.com>
2022-08-11 21:06:25 +0900

    1. 내리막길 2. 타일채우기 3. 동전2

---
+++ CodingTest/src/main/java/codingtest/baekjoon/DP/타일채우기.java
@@ -0,0 +1,34 @@
+package codingtest.baekjoon.DP;
+
+import java.util.Scanner;
+
+public class 타일채우기 {
+
+	public static void main(String[] args) {
+		Scanner sc 	= new Scanner(System.in);
+        int n 		= sc.nextInt();
+        int[] dp 	= new int[n + 1];
+
+        int answer = 0;
+        if (n % 2 == 1) {
+            answer = 0;
+        } else {
+            dp[0] = 1;
+            dp[2] = 3;
+
+            for (int i = 4; i <= n; i += 2) {
+                dp[i] = dp[i - 2] * dp[2];
+                for (int j = i - 4; j >= 0; j -= 2) {
+                    dp[i] += dp[j] * 2;
+                }//end for()
+            }//end for()
+
+            answer = dp[n];
+        }//end id()
+
+        System.out.println(answer);
+        sc.close();
+
+	}//end main()
+
+}//end class()
