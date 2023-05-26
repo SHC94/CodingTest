00424a0a03cb8981562e91222d380749239ae81f
SHC94 <gudcjf4757@naver.com>
2023-05-26 17:21:42 +0900

    1. 그대로출력하기 2. 문자열 3. 문자와문자열 4. 진법변환2 5. 진법변환 6. 팰린드롬인지확인하기 7. 너의평점은(예정)

---
+++ CodingTest/src/main/java/codingtest/baekjoon/String/그대로출력하기.java
@@ -0,0 +1,17 @@
+package codingtest.baekjoon.String;
+
+import java.util.Scanner;
+
+public class 그대로출력하기 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+		    System.out.println(sc.nextLine());
+		}
+		
+	}
+	
+}
