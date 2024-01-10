d2bfdb9e96d8b900512d91c7b9b291e424cec84a
SHC94 <gudcjf4757@naver.com>
2024-01-10 14:50:32 +0900

    1. 과제 안 내신 분..?

---
+++ CodingTest/src/main/java/codingtest/baekjoon/Array/과제안내신분.java
@@ -0,0 +1,22 @@
+package codingtest.baekjoon.Array;
+
+import java.util.Scanner;
+
+public class 과제안내신분 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc 		= new Scanner(System.in);
+		int[] student 	= new int[30];
+
+		for (int a = 0 ; a < 28 ; a++) {
+			int n = sc.nextInt();
+			student[n - 1] = 9;
+		}
+
+		for(int a = 0 ; a < student.length ; a++) {			
+			if(student[a] != 9) System.out.println(a + 1);
+		}
+		
+    }
+}
