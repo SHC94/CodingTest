9696d4f2bc129f835721efba947078337fba22d5
SHC94 <gudcjf4757@naver.com>
2024-01-10 11:08:36 +0900

    GIT AUHTOR 변

--- CodingTest/src/main/java/codingtest/baekjoon/Avatar/단어길이재기.java
+++ CodingTest/src/main/java/codingtest/baekjoon/Avatar/단어길이재기.java
@@ -1,6 +1,10 @@
 package codingtest.baekjoon.Avatar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import org.springframework.util.CollectionUtils;
 
 public class 단어길이재기 {
 	
@@ -8,6 +12,10 @@ public class 단어길이재기 {
 		Scanner sc	= new Scanner(System.in);
 		String S 	= sc.next();
 		System.out.println(S.length());
+		String.valueOf(false);
+		
+		List<String> list = new ArrayList<>();
+		CollectionUtils.isEmpty(list);
 	}//end main()
 
 }//end class()
