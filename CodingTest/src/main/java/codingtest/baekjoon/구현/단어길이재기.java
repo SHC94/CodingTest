2a4705add94e8d64630115e9a045d70846b68e14
hshin <hshin@hist.co.kr>
2024-07-30 11:00:26 +0900

    test

--- CodingTest/src/main/java/codingtest/baekjoon/구현/단어길이재기.java
+++ CodingTest/src/main/java/codingtest/baekjoon/구현/단어길이재기.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 import org.springframework.util.CollectionUtils;
 
 public class 단어길이재기 {
-	
+
 	public static void main(String[] args) {
-		Scanner sc	= new Scanner(System.in);
-		String S 	= sc.next();
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
 		System.out.println(S.length());
 		String.valueOf(false);
-		
+
 		List<String> list = new ArrayList<>();
-		CollectionUtils.isEmpty(list);
+		CollectionUtils.isEmpty(list); // test
 	}//end main()
 
 }//end class()
